250cb814abe9f2a1c1e4bd77fceecc0b712bfb4e
Kofort9 <kofort9@gmail.com>
2021-04-08 13:12:37 -0400

    Updated login page

--- src/components/LoginDashboard.js
+++ src/components/LoginDashboard.js
@@ -6,94 +6,52 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import {Helmet} from 'react-helmet';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { MDBContainer, MDBRow, MDBCol } from "mdbreact"
 
-
-// Sign in with SignInwithPopup
-// firebase.auth().signInwithPopup(provider)
-// .then((result) => {
-//   /** @type {firebase.auth.OAuthCredential} */
-//   var credential = result.credential;
-//
-//   // This gives you a Google Access Token. You can use it to access the Google API.
-//   var token = credential.accessToken;
-//
-//   // The signed-in user info.
-//   var user = result.user;
-//
-//
-// }).catch((error) => {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var  errorMessage = error.message;
-//   // The email of the user's account used.
-//   var email = error.email;
-//   // The firebase.auth.AuthCredential type that was used.
-//   var credential = error.credential;
-// });
-
-// Sing in with Redirect
-
-
-
-
-// firebase.auth()
-//   .getRedirectResult()
-//   .then((result) => {
-//     if (result.credential) {
-//       /** @type {firebase.auth.OAuthCredential} */
-//       var credential = result.credential;
-//
-//       // This gives you a Google Access Token. You can use it to access the Google API.
-//       var token = credential.accessToken;
-//       // ...
-//     }
-//     // The signed-in user info.
-//     var user = result.user;
-//   }).catch((error) => {
-//     // Handle Errors here.
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     // The email of the user's account used.
-//     var email = error.email;
-//     // The firebase.auth.AuthCredential type that was used.
-//     var credential = error.credential;
-//     // ...
-//   });
 
 
 
 export default function LoginDashboard() {
-   //initialize firebase
 
-
-  // var firebaseConfig = {
-  //   apiKey: "AIzaSyAUSMd4BKdxKIIgq2VGLwfzLPXW-wLzBkc",
-  //   authDomain: "hu-drive.firebaseapp.com",
-  //   projectId: "hu-drive",
-  //   storageBucket: "hu-drive.appspot.com",
-  //   messagingSenderId: "870378528473",
-  //   appId: "1:870378528473:web:32fd812371f35bb746e8c8",
-  //   measurementId: "G-TFBDHN9TZ4"
-  // }
-  // firebase.initializeApp(firebaseConfig);
-
-  // firebase.initializeApp(firebaseConfig);
   var provider = new firebase.auth.GoogleAuthProvider();
-  function sendToLoginSuccesssful() {
+
+  function sendToLoginSuccesssful(provider) {
+   console.log('Successful');
    firebase.auth().signInWithRedirect(provider);
-    console.log('Successful');
+   console.log('LOG');
+     firebase.auth().onAuthStateChanged(function(user) {
+     if (user) {
+       console.log('User is signed in.');
+     } else {
+       console.log('No user is signed in.');
+     }
+   });
   }
+    firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log('User is signed in.');
+    } else {
+      console.log('No user is signed in.');
+    }
+  });
+  console.log('Line 37'); 
 
     return(
-      
+
         <div>
             <Helmet>
                 <style>{'body { background-color: #357EC7; }'}</style>
             </Helmet>
         <h1>Login </h1>
-        <button onClick={() => {firebase.auth().signInWithRedirect(provider)}}
+        <button onClick={() => {sendToLoginSuccesssful(provider)}}
             type="button" class="btn">Enter</button>
+        <Link to="/main" type="button" class="btn">Go to Dashboard</Link>
           <MDBContainer className="mt-5">
         <MDBRow className="mb-4">
           <MDBCol md="8">
