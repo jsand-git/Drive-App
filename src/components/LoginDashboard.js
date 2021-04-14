d05761aa14b601ef4d92bf337e08bde267f4c6d0
JoedNgangmeni <joed.ngangmeni@bison.howard.edu>
2021-04-13 21:32:37 -0400

    changed button of button to just button (login)

--- src/components/LoginDashboard.js
+++ src/components/LoginDashboard.js
@@ -40,12 +40,12 @@ export default function LoginDashboard() {
       <img src= "/loginImgs/HUBison.svg" alt= "Bison Logo" style={{ float: 'left', paddingRight : '5px' }} />
       <h1 style={{color: "tomato",fontSize: 70, fontWeight: "bolder"}}> LOGIN  </h1>
 
-
+{/* 
         <button onClick={() => {sendToLoginSuccesssful(provider)}}
             type="button" class="btn">
             <Link to="/main" type="button" class="btn">Enter</Link>
-        </button>
-
+        </button> */}
+        <Link to="/main" type="button" class="btn" onClick={() => {sendToLoginSuccesssful(provider)}}>Enter</Link>
 
       <img src= "/loginImgs/HUBisonWords.png" alt= "Bison Logo Words" style={{ width: 600 , height: 300, left: 850, bottom: 475,  position: "absolute"}} />
       <img src= "/loginImgs/GDRLogo.png" alt= "Google Drive Logo" style={{ width: 400 , height: 400, left: 925, bottom: 50, position: "absolute"}} />
