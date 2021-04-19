64401daffa8d2a7449d252c1cbd96cb605b0d5a7
Kofort9 <kofort9@gmail.com>
2021-04-19 19:29:03 -0400

    Fixed login button CSS

--- src/components/LoginDashboard.js
+++ src/components/LoginDashboard.js
@@ -6,7 +6,7 @@ import "firebase/auth";
 import "firebase/firestore";
 import {Helmet} from 'react-helmet';
 import {
-  Redirect, 
+  Redirect,
   BrowserRouter as Router,
   Switch,
   Route,
@@ -24,7 +24,7 @@ export default function LoginDashboard() {
   var provider = new firebase.auth.GoogleAuthProvider();
   var user = firebase.auth().currentUser;
 
-  
+
   function sendToLoginSuccesssful(provider) {
   var user = firebase.auth().currentUser;
   if (user == null){
@@ -34,7 +34,7 @@ export default function LoginDashboard() {
     .then((result) => {
       /** @type {firebase.auth.OAuthCredential} */
       var credential = result.credential;
-  
+
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = credential.accessToken;
       // The signed-in user info.
@@ -53,11 +53,11 @@ export default function LoginDashboard() {
   }
 
    return <Redirect to="/main"></Redirect>;
-  
+
 }
 
-  
-  
+
+
 
     return(
 
@@ -65,7 +65,7 @@ export default function LoginDashboard() {
       <img src= "/loginImgs/HUBison.svg" alt= "Bison Logo" style={{ float: 'left', paddingRight : '5px' }} />
       <h1 style={{color: "tomato",fontSize: 70, fontWeight: "bolder"}}> LOGIN  </h1>
 
-        <button onClick={() => {sendToLoginSuccesssful(provider)}}>Enter</button>
+        <button onClick={() => {sendToLoginSuccesssful(provider)}} type="button" class="btn">Enter</button>
 
       <img src= "/loginImgs/HUBisonWords.png" alt= "Bison Logo Words" style={{ width: 600 , height: 300, left: 850, bottom: 475,  position: "absolute"}} />
       <img src= "/loginImgs/GDRLogo.png" alt= "Google Drive Logo" style={{ width: 400 , height: 400, left: 925, bottom: 50, position: "absolute"}} />
